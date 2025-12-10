cdff6db1842959e361dff47d222d1b031277471e
Tania Kolesnik <kolesnik@gmail.com>
2025-12-10 19:08:05 +0000

    update task.sql added fixed statement

--- task.sql
+++ task.sql
@@ -2,12 +2,12 @@ USE ShopDB;
 
 DELIMITER //
 CREATE PROCEDURE get_warehouse_product_inventory(
-  IN WarehouseID varchar(50)
+  IN Warehouse_ID varchar(50)
 )
 BEGIN
   SELECT Products.Name, ProductInventory.WarehouseAmount 
   FROM ProductInventory 
   JOIN Products on Products.ID = ProductInventory.ProductID
-  WHERE ProductInventory.WarehouseID = WarehouseID
+  WHERE ProductInventory.WarehouseID = Warehouse_ID;
 END //
 DELIMITER ;
