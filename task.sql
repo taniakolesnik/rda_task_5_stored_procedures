b73552de927214995047ffc6b6d6486ce50c69cc
Tania Kolesnik <kolesnik@gmail.com>
2025-12-10 18:57:49 +0000

    update task.sql

--- task.sql
+++ task.sql
@@ -1,3 +1,12 @@
 USE ShopDB; 
 
--- Create your stored procedure here
+DELIMITER //
+CREATE PROCEDURE get_warehouse_product_inventory(
+  IN WarehouseID varchar(50)
+)
+BEGIN
+  SELECT Products.Name, ProductInventory.WarehouseAmount 
+  FROM ProductInventory 
+  JOIN Products on Products.ID = ProductInventory.ProductID;
+END //
+DELIMITER ;
